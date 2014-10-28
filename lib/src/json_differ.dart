a94715ee962701dfd15ced7d04dd6f3e7676a36c
Sam Rawlins <srawlins@google.com>
2014-10-28 10:25:01 -0700

    Apache license at top of each file.

--- lib/src/json_differ.dart
+++ lib/src/json_differ.dart
@@ -1,3 +1,6 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0, found in the LICENSE file.
+
 part of json_diff;
 
 class JsonDiffer {
--- test/json_diff_test.dart
+++ test/json_diff_test.dart
@@ -1,3 +1,6 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0, found in the LICENSE file.
+
 /// Unit tests for json_diff.
 library json_diff_tests;
 
